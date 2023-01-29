c484fc346462964124108458b36c0a4b26c80bd4
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-01-29 20:00:21 +0530

    c file

---
+++ booleans.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int myInt=5;
+    int myint=10;
+    printf("%d\n",myInt>myint);
+    printf("%d\n",myInt==myint);
+    printf("%d\n",myInt==5);
+    return 0;
+}
