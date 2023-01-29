3f3d95221f00e03dc9cff67aa6fe17dbeab1898f
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-01-29 20:17:56 +0530

    c file

---
+++ if..else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int x=5;
+    int y=10;
+    if (x>y) {
+        printf("Yes x is greater than y.");
+    }else if(x<y){
+        printf("Yes x is smaller than y.");
+
+    }else{
+        printf("No");
+    }
+}
