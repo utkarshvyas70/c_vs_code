c3c7a98c0006c3a8a2d4992a438b62cef8d59f12
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-01-29 19:56:27 +0530

    c file

---
+++ operator.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main(){
+    // int sum1=100+50;
+    // int sum2=sum1+10;
+    // int sum3=sum2+sum1;
+    // printf("%d\n",sum3);
+
+
+
+    // int x=5;
+    // int y=2;
+    // printf("%d\n",x%y);
+    // printf("%d\n",++x);
+    // printf("%d\n",--x);
+
+    // x%=3;
+    // x&=3;
+    // x|=3;
+    // x^=3;
+    // x>>=3;
+    // printf("%d\n",x);
+
+
+
+    // printf("%d\n",x==y);
+    // printf("%d\n",x!=y);
+    // printf("%d\n",x>y);
+    // printf("%d\n",x<y);
+    // printf("%d\n",x>=y);
+    // printf("%d/n",x<=y);
+    int myInt;
+    float myFloat;
+    double myDouble;
+    char myChar;
+    printf("%lu\n",sizeof(myInt));
+    printf("%lu\n",sizeof(myFloat));
+    printf("%lu\n",sizeof(myDouble));
+    printf("%lu\n",sizeof(myChar));
+
+    // printf("%d\n",x>2 && x<10);
+    return 0;
+}
